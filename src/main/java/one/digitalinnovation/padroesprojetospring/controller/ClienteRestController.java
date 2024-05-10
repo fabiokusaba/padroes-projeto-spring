17609662aa8b0b58844521dd847e86cb00059441
fabiokusaba <fabinhokb44@gmail.com>
2024-05-10 12:09:21 -0300

    add: anotação @Service na classe ClienteServiceImpl

---
+++ src/main/java/one/digitalinnovation/padroesprojetospring/controller/ClienteRestController.java
@@ -0,0 +1,4 @@
+package one.digitalinnovation.padroesprojetospring.controller;
+
+public class ClienteRestController {
+}
--- src/main/java/one/digitalinnovation/padroesprojetospring/service/impl/ClienteServiceImpl.java
+++ src/main/java/one/digitalinnovation/padroesprojetospring/service/impl/ClienteServiceImpl.java
@@ -2,7 +2,9 @@ package one.digitalinnovation.padroesprojetospring.service.impl;
 
 import one.digitalinnovation.padroesprojetospring.model.Cliente;
 import one.digitalinnovation.padroesprojetospring.service.ClienteService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ClienteServiceImpl implements ClienteService {
     @Override
     public Iterable<Cliente> buscarTodos() {
