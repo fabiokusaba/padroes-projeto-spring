ec91ed369a78d14d415661617f63e31fba74d294
fabiokusaba <fabinhokb44@gmail.com>
2024-05-10 11:57:08 -0300

    add: repositories ClienteRepository e EnderecoRepository

---
+++ src/main/java/one/digitalinnovation/padroesprojetospring/model/ClienteRepository.java
@@ -0,0 +1,6 @@
+package one.digitalinnovation.padroesprojetospring.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+}
---
+++ src/main/java/one/digitalinnovation/padroesprojetospring/model/EnderecoRepository.java
@@ -0,0 +1,6 @@
+package one.digitalinnovation.padroesprojetospring.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EnderecoRepository extends CrudRepository<Endereco, Long> {
+}
